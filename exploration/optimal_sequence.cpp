8a59f0fc11c5be991f5681a3a62af94c7d54395b
aijac0 <aidancollinscs@gmail.com>
2022-11-08 11:05:27 -0500

    Initialized file

---
+++ exploration/optimal_sequence.cpp
@@ -0,0 +1,22 @@
+// File: optimal_sequence.cpp
+// Author: Aidan Collins
+// Github: aijac0
+// Email: aidancollinscs@gmail.com
+// The goal of this file is to find an algorithm for creating a sequence of
+// integer which encompasses all possible values, and where each consecutive 
+// integer has only one changed bit from the previous.
+// // // // // // // // // // // // // // // // // // // // // // // // // //
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+
+
+
+int main()
+{
+
+}
