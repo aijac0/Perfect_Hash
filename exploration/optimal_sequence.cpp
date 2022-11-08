485bbb8b61cbed16c21160d55110f9a9b74fd9f4
aijac0 <aidancollinscs@gmail.com>
2022-11-08 11:16:54 -0500

    Implemented get_neighbors(); creates an array of all the integers with one bit flipped from the input integer

--- exploration/optimal_sequence.cpp
+++ exploration/optimal_sequence.cpp
@@ -14,9 +14,24 @@
 using namespace std;
 
 
+template <typename IntType>
+vector<IntType> possible_neighbors(IntType node)
+{
+	vector<IntType> neighbors(sizeof(IntType) * 8); 	// Initialize neighbors vector to include all variations
+	for (int i = 0; i < sizeof(IntType) * 8; i++)
+	{
+		neighbors[i] = node ^ (1 << i);					// Set the element at each index as node with its ith bit from the right flipped	
+	}
+	return neighbors;
+}
+
 
 
 int main()
 {
-
+	unsigned int node = 15;
+	for (unsigned int variation : possible_neighbors(node))
+	{
+		cout << variation << endl;
+	}
 }
