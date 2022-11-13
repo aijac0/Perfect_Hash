70c693c39e54fee96b6172eff72a5bba1a30d74a
aijac0 <aidancollinscs@gmail.com>
2022-11-12 23:51:26 -0500

    Implemented sort_expressions() and get_rank()

--- src/qmc_minimization.cpp
+++ src/qmc_minimization.cpp
@@ -5,3 +5,81 @@
 // Efficient implementation of the Quine-McCluskey
 // boolean expression minimization algorithm
 // // // // // // // // // // // // // // // // //
+
+#include <vector>
+
+using namespace std;
+
+
+/**
+ * Get the rank of a boolean expression
+ * Rank is defined as the number of non-negated variables.
+ * For example:
+ * 		rank(!A * B * C) = rank(011) = 2
+ *		rank(!A * !B * !C) = rank(000) = 0
+ *		rank(A * B * C) = rank(111) = 3
+ * @param exp: Boolean expression to get the rank of
+ * @return: Rank of exp
+ */
+int get_rank(size_t exp)
+{
+	int rank = 0;
+    while exp > 0
+	{
+       	rank++;
+        exp = exp & (exp-1);
+	}
+    return rank;
+}
+
+
+/**
+ * Sort the expressions vector by rank in ascending order
+ * Sorting algorithm takes advantage of fact that there is a relatively
+ * small number of ranks, and the range of possible ranks is known.
+ * @param expressions: Expressions vector to be sorted
+ * @return: Vector of indices in expressions vector which indicate the starting index
+ *          of a rank in expressions specified by the index of the vector itself
+ */
+vector<size_t> & sort_by_rank(vector<size_t> & expressions)
+{
+	size_t n_in_rank[sizeof(size_t) + 1] = {0};
+	
+	// Count how many expressions there are of each rank 
+	// (to find where to place them in sorted vector)
+	for (size_t i = 0; i < expressions.size(); i++)
+	{
+		n_in_rank[get_rank(expressions[i])]++;
+	}
+
+	// For an index i, separation_indices[i] represents the starting
+	// index of a specified rank in the expressions vector
+	vector<size_t>* separation_indices(sizeof(size_t) + 1, 0);
+	for (int i = 0; i < sizeof(size_t); i++)
+	{
+		separation_indices[i+1] = separation_indices[i] + n_in_rank[i];
+	}
+	
+	// Set n_in_rank equal to separation_indices to mark the current index
+	// in expressions for the next expression of rank i
+	for (int i = 0; i <= sizeof(size_t); i++)
+	{
+		n_in_rank[i] = separation_indices[i];
+	}
+
+	// Initialize a new vector as the sorted version of expressions
+	vector<size_t> sorted_expressions(expressions.size(), 0);
+	for (size_t i = 0; i < expressions.size(); i++)
+	{
+		int rank = get_rank(expressions[i]);
+		sorted_expressions[n_in_rank[rank]++] = expressions[i];
+	}
+
+	// Set expressions equal to sorted_expressions
+	for (size_t i = 0; i < expressions.size(); i++)
+	{
+		expressions[i] = sorted_expressions[i];
+	}
+
+	return separation_indices;
+}
